a6062f09014204975554e57bb294579e29a174cb
zilehuda tariq <zilehuda.tariq@gmail.com>
2022-09-07 00:58:57 +0500

    👌IMPROVE: Changed the API urls

--- djaas/urls.py
+++ djaas/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.authentications.urls")),
+    path("api/", include("apps.authentications.api.urls")),
 ]
