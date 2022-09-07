067889fcc5e0de87e652fcbb244d34c24887fc56
zilehuda tariq <zilehuda.tariq@gmail.com>
2022-09-07 05:16:50 +0500

    📦NEW: Added subscription property with alway return True

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,3 +5,7 @@ from utils.db.models import BaseModel
 
 class User(AbstractUser, BaseModel):
     email = models.EmailField(unique=True)
+
+    @property
+    def is_subscription_active(self):
+        return True
