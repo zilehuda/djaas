8d6e70fd73b15f7104a72f5808c1a85f7bb4f5fa
zilehuda <zilehuda.tariq@gmail.com>
2023-03-13 04:11:21 +0500

    login_required for a swagger

--- djaas/urls.py
+++ djaas/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.auth.decorators import login_required
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,7 +37,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path(
         "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
+        login_required(schema_view.with_ui("swagger", cache_timeout=0)),
         name="schema-swagger-ui",
     ),
     path("api/auth/", include("apps.authentications.api.urls")),
